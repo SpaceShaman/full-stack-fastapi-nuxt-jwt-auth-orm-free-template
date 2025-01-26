ee98ffe41163fee5451ba1d4bba774d1feba572e
SpaceShaman <spaceshaman@tuta.io>
2025-01-26 18:29:40 +0100

    ✨ (main.py): add database migration support using asynccontextmanager
    ⬆️ (pyproject.toml): update fastapi dependency to include 'standard' extras

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from products.api import router as products_router
+from sqlift import up
 
-app = FastAPI()
+
+@asynccontextmanager
+async def migrate(app: FastAPI):
+    up()
+    yield
+
+
+app = FastAPI(lifespan=migrate)
 
 
 app.include_router(products_router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app)
