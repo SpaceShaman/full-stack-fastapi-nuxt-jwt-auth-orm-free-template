24baa0e80874d63ee6d432781ffc8b0843371e04
SpaceShaman <spaceshaman@tuta.io>
2025-01-28 15:37:08 +0100

    ♻️ (auth): refactor password hashing and verification into AuthService class
    Move password hashing and verification logic from utils.py to AuthService
    class to encapsulate authentication logic within the service. This change
    improves code organization and maintainability by keeping related
    functionality together.

--- app/auth/services.py
+++ app/auth/services.py
@@ -1,11 +1,11 @@
 from typing import Protocol
 
+from passlib.context import CryptContext
 from users.models import UserWithPassword
 from users.repositorys import UserRepository
 
 from .exceptions import IncorrectUsernameOrPassword
 from .models import Token
-from .utils import verify_password
 
 
 class UserRepositoryInterface(Protocol):
@@ -15,9 +15,16 @@ class UserRepositoryInterface(Protocol):
 class AuthService:
     def __init__(self) -> None:
         self.user_repository: UserRepositoryInterface = UserRepository()
+        self.pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
     def login(self, username: str, password: str) -> Token:
         user = self.user_repository.get_user_with_password(username)
-        if not user or not verify_password(password, user.password):
+        if not user or not self._verify_password(password, user.password):
             raise IncorrectUsernameOrPassword("Incorrect username or password")
         return Token(access_token="token", token_type="bearer")
+
+    def _generate_password_hash(self, password: str) -> str:
+        return self.pwd_context.hash(password)
+
+    def _verify_password(self, plain_password, hashed_password):
+        return self.pwd_context.verify(plain_password, hashed_password)
