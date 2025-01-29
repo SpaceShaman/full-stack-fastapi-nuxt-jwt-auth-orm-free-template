8fd3764d5513f956ff52f13689417e832f986172
SpaceShaman <spaceshaman@tuta.io>
2025-01-29 15:36:40 +0100

    ♻️ (dependencies.py): refactor token decoding logic into a separate function _decode_token for improved readability and maintainability 🐛 (dependencies.py): fix user retrieval to handle UserNotFound exception and return appropriate HTTP error response

--- app/users/dependencies.py
+++ app/users/dependencies.py
@@ -13,12 +13,15 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme)) -> User:
     try:
-        payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=["HS256"])
+        return UserService().get_user(_decode_token(token).get("sub"))
+    except UserNotFound as exc:
+        raise HTTPException(status_code=401, detail="User not found") from exc
+
+
+def _decode_token(token: str) -> dict:
+    try:
+        return jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=["HS256"])
     except jwt.ExpiredSignatureError as e:
         raise HTTPException(status_code=401, detail="Token has expired") from e
     except jwt.InvalidTokenError as e:
         raise HTTPException(status_code=401, detail="Invalid token") from e
-    try:
-        return UserService().get_user(payload.get("sub"))
-    except UserNotFound as exc:
-        raise HTTPException(status_code=401, detail="User not found") from exc
