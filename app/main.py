4f492b39d70aec212b81a5908aa2c0f58123e65b
SpaceShaman <spaceshaman@tuta.io>
2025-02-05 18:09:20 +0100

    🔥 (main.py, migrations, products): remove product feature and related database table
    Remove the products feature, including its API, models, services, and tests.
    Delete the migration file for the products table. This change is made to
    simplify the application by focusing on user-related features and removing
    unused or unnecessary components.

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from contextlib import asynccontextmanager
 
 from auth.api import auth_router
 from fastapi import FastAPI
-from products.api import products_router
 from sqlift import up
 from users.api import users_router
 
@@ -16,7 +15,6 @@ app = FastAPI(lifespan=migrate)
 
 app.include_router(auth_router)
 app.include_router(users_router)
-app.include_router(products_router)
 
 if __name__ == "__main__":
     import uvicorn
--- app/products/models.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class ProductOut(BaseModel):
-    id: int
-    name: str
-    price: float
-    stock: int
