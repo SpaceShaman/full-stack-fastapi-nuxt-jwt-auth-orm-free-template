e24641f340a4adca2e50077e9aadff735c4458f0
SpaceShaman <spaceshaman@tuta.io>
2025-02-07 16:51:32 +0100

    ✨ (auth): add new authentication components for login, registration, and password reset forms
    ♻️ (auth): refactor authentication page to use AuthTabs component for better modularity and maintainability
    
    🔥 (auth): remove old login page in favor of new modular authentication components

---
+++ frontend/app/components/auth/AuthTabs.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+const forgotPasswordTab = ref(false);
+</script>
+<template>
+  <div role="tablist" class="w-full max-w-sm tabs tabs-lifted">
+    <input
+      type="radio"
+      name="tabs"
+      role="tab"
+      class="tab"
+      aria-label="Login"
+      checked
+      @click="forgotPasswordTab = false"
+    />
+    <div
+      role="tabpanel"
+      class="tab-content bg-base-100 border-base-300 rounded-box shadow-2xl"
+    >
+      <AuthLoginForm
+        v-if="!forgotPasswordTab"
+        @forgot-password="forgotPasswordTab = true"
+      />
+      <AuthForgotPasswordForm v-else />
+    </div>
+
+    <input
+      type="radio"
+      name="tabs"
+      role="tab"
+      class="tab"
+      aria-label="Register"
+      @click="forgotPasswordTab = false"
+    />
+    <div
+      role="tabpanel"
+      class="tab-content bg-base-100 border-base-300 rounded-box shadow-2xl"
+    >
+      <AuthRegisterForm />
+    </div>
+  </div>
+</template>
---
+++ frontend/app/pages/auth.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hero bg-base-200 min-h-screen">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Login now!</h1>
+        <p class="py-6">
+          Hi, I am SpaceShaman and I have prepared for you this full stack
+          aplication template using Fastapi, Nuxt, TailwindCSS, DaisyUI, SQLite,
+          JWT authentiation. You can find the source code and more interesting
+          projects on my github
+          <a class="link-hover" href="https://github.com/SpaceShaman"
+            >github.com/SpaceShaman</a
+          >
+        </p>
+      </div>
+      <AuthTabs />
+    </div>
+  </div>
+</template>
