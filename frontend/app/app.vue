c9d898d06488cce69fad10b10aee0b3b15515741
SpaceShaman <spaceshaman@tuta.io>
2025-02-07 12:54:20 +0100

    ✨ (app.vue): add ThemeSelector component for theme selection
    ✨ (ThemeInput.vue): create ThemeInput component for theme options
    
    ✨ (ThemeSelector.vue): create ThemeSelector component with theme list
    
    ⬆️ (package.json): add @tailwindcss/typography dependency
    
    🔧 (tailwind.config.js): configure daisyui themes and typography plugin

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,1 +1,5 @@
-<template><h1 class="text-3xl font-bold underline">Hello world!</h1></template>
+<template>
+  <ThemeSelector class="absolute top-0 right-0 m-1" />
+
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
---
+++ frontend/app/components/ThemeInput.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ name: string }>();
+</script>
+
+<template>
+  <li>
+    <input
+      type="radio"
+      name="theme-dropdown"
+      class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+      :aria-label="name.charAt(0).toUpperCase() + name.slice(1)"
+      :value="name"
+    />
+  </li>
+</template>
