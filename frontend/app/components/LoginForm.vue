646c629e332ddbc0531220a4929c2d2650770691
SpaceShaman <spaceshaman@tuta.io>
2025-02-07 14:15:20 +0100

    ✨ (LoginForm.vue): add new LoginForm component for user authentication
    ♻️ (login.vue): refactor login page to use the new LoginForm component

---
+++ frontend/app/components/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="card-body">
+    <div class="form-control">
+      <label class="label">
+        <span class="label-text">Username</span>
+      </label>
+      <input
+        type="text"
+        placeholder="Username"
+        class="input input-bordered"
+        required
+      />
+    </div>
+    <div class="form-control">
+      <label class="label">
+        <span class="label-text">Password</span>
+      </label>
+      <input
+        type="password"
+        placeholder="password"
+        class="input input-bordered"
+        required
+      />
+      <label class="label">
+        <a href="#" class="label-text-alt link link-hover">Register</a>
+      </label>
+    </div>
+    <div class="form-control mt-6">
+      <button class="btn btn-primary">Login</button>
+    </div>
+  </form>
+</template>
--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -12,36 +12,7 @@
         </p>
       </div>
       <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form class="card-body">
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              placeholder="email"
-              class="input input-bordered"
-              required
-            />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              class="input input-bordered"
-              required
-            />
-            <label class="label">
-              <a href="#" class="label-text-alt link link-hover">Register</a>
-            </label>
-          </div>
-          <div class="form-control mt-6">
-            <button class="btn btn-primary">Login</button>
-          </div>
-        </form>
+        <LoginForm />
       </div>
     </div>
   </div>
