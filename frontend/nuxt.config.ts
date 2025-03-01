e2fd814acff642251a4fbcb89ea480c85e8b3954
SpaceShaman <spaceshaman@tuta.io>
2025-03-01 19:22:13 +0100

    🔧 (frontend): add Prettier and ESLint configuration files
    Add .prettierrc for consistent code formatting and eslint.config.mjs
    for linting. This ensures code quality and style consistency across
    the project.
    
    🔧 (nuxt.config.ts): integrate ESLint module
    
    Include '@nuxt/eslint' in the Nuxt configuration to enable linting
    support, improving code quality and catching potential issues early.
    
    ⬆️ (package.json): add Prettier, ESLint, and related plugins
    
    Add Prettier and ESLint dependencies along with scripts for linting
    and formatting. This enhances code quality and maintains a consistent
    coding style across the project.

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,33 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  srcDir: "app",
-  compatibilityDate: "2024-11-01",
-  devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxtjs/color-mode",
-    "@vee-validate/nuxt",
-    "@pinia/nuxt",
-    "pinia-plugin-persistedstate",
-  ],
-  colorMode: {
-    preference: "system",
-    fallback: "dark",
-    dataValue: "theme",
-  },
-  components: [
-    {
-      path: "~/components",
-    },
-    {
-      path: "~/components/ui",
-      pathPrefix: false,
-    },
-  ],
-  piniaPluginPersistedstate: {
-    storage: "cookies",
-    cookieOptions: {
-      sameSite: "lax",
-    },
-  },
-});
+	srcDir: 'app',
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@nuxtjs/color-mode',
+		'@vee-validate/nuxt',
+		'@pinia/nuxt',
+		'pinia-plugin-persistedstate',
+		'@nuxt/eslint',
+	],
+	colorMode: {
+		preference: 'system',
+		fallback: 'dark',
+		dataValue: 'theme',
+	},
+	components: [
+		{
+			path: '~/components',
+		},
+		{
+			path: '~/components/ui',
+			pathPrefix: false,
+		},
+	],
+	piniaPluginPersistedstate: {
+		storage: 'cookies',
+		cookieOptions: {
+			sameSite: 'lax',
+		},
+	},
+})
