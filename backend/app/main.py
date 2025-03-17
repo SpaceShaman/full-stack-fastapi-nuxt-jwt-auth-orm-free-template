0ce931e97dee257fae8d6abcb4d08a844a132888
SpaceShaman <spaceshaman@tuta.io>
2025-03-17 17:38:55 +0100

    🔧 (docker-compose): move database migration to docker-compose command
    ♻️ (main.py): remove asynccontextmanager for migration
    
    Remove the asynccontextmanager for database migration in `main.py`
    and shift the migration command to `docker-compose` files. This
    ensures that migrations are run as part of the container startup
    process, improving deployment consistency and simplifying the
    application's startup logic.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,6 @@
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
-from sqlift import up
 from users.router import router
 
-
-@asynccontextmanager
-async def migrate(app):
-    yield up(migrations_path="database/migrations")
-
-
-app = FastAPI(lifespan=migrate, root_path="/api")
+app = FastAPI(root_path="/api")
 
 app.include_router(router)
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app)
