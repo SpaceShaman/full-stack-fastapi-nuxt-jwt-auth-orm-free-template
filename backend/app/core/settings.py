9f273d45bef074c62edf6cbef05dbf4d16ab9b09
SpaceShaman <spaceshaman@tuta.io>
2025-03-24 17:32:54 +0100

    🔧 (env): add DEBUG variable to .env.default for development configuration
    ✨ (settings.py): introduce DEBUG setting to toggle debug mode
    
    ✨ (main.py): enable conditional debug mode and documentation routes based on DEBUG setting

--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -2,6 +2,7 @@ from os import getenv
 
 DB_URL = getenv("DB_URL", "sqlite:///db.sqlite")
 SECRET_KEY = getenv("SECRET_KEY", "secret")
+DEBUG = getenv("DEBUG", "False").lower() == "true"
 SMTP_HOST = getenv("SMTP_HOST", "smtp.gmail.com")
 SMTP_PORT = int(getenv("SMTP_PORT", 465))
 SMTP_USER = getenv("SMTP_USER", "")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,15 @@
 from auth.router import auth_router
-from core.settings import BASE_URL
+from core.settings import BASE_URL, DEBUG
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from users.router import users_router
 
-app = FastAPI(root_path="/api")
+app = FastAPI(
+    root_path="/api",
+    debug=DEBUG,
+    openapi_url="/openapi.json" if DEBUG else None,
+    docs_url="/docs" if DEBUG else None,
+)
 
 app.add_middleware(
     CORSMiddleware,
