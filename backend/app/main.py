4424d60998b43d2bd4eb473a63b28003a6c7bc59
SpaceShaman <spaceshaman@tuta.io>
2025-03-24 17:28:15 +0100

    ✨ (main.py): add CORS middleware to allow requests from specified origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from auth.router import auth_router
+from core.settings import BASE_URL
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from users.router import users_router
 
 app = FastAPI(root_path="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[f"http://{BASE_URL}", f"https://{BASE_URL}"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router)
 app.include_router(auth_router)
